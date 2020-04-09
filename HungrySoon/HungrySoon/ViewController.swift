27901488f2d75f13b0309565614d039e3ffb5a30
mistersquiish <henryvuong@utexas.edu>
2020-04-09 14:02:26 -0500

    first commit

---
+++ HungrySoon/HungrySoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HungrySoon
+//
+//  Created by Henry Vuong on 4/9/20.
+//  Copyright © 2020 Henry Vuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HungrySoon
+
+App that notifies users when their favorite restaurants are closing soon.
+
+Henry Vuong - Lead Developer
+Savong Tan - Lead Designer
