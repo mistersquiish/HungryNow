3132228a121eb4a5a1193fbcd25d9c56c656e3e3
mistersquiish <henryvuong@utexas.edu>
2020-04-09 19:16:15 -0500

    added storyboards

--- HungryNow/HungryNow/View Controllers/AddVC.swift
+++ HungryNow/HungryNow/View Controllers/AddVC.swift
@@ -11,14 +11,31 @@ import UIKit
 class AddVC: UIViewController {
 
     @IBOutlet weak var popUpContainer: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var cancelButtonOutlet: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // delegates
+//        tableView.delegate = self
+//        tableView.dataSource = self
         
         let searchController = UISearchController(searchResultsController: nil)
         popUpContainer.addSubview(searchController.searchBar)
 
     }
     
-
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return
+//    }
+    
 }
